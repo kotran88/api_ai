c2539413ff48159df0415942aab3318e9fc6dab9
정평두 <kotran@naver.com>
2018-07-02 15:09:35 +0900

    update

--- index.js
+++ index.js
@@ -284,7 +284,7 @@ app.get('/success',function(req,res){
     
             title=snap.val().title;
             sendMessage(appId,snap.val().title+" 의 버튼이 눌러졌습니다!");
-            var ref = firebase.database().ref().child('clients').child(phoneKey).child("devices").child(device).child("record");
+            var ref = firebase.database().ref().child('clients').child(phoneKey).child("devices").child(device).child("Record");
 
             var messageRef= ref;
           
